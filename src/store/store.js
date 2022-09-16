ba1ef7930db0b389acc4bd265ef0d181c0b463b7
Richard Fu <richardfxr.personal@gmail.com>
2022-09-16 01:35:19 -0400

    🟠SVLT: added period store & updated clock

--- src/store/store.js
+++ src/store/store.js
@@ -8,25 +8,45 @@ export const minutes = writable(0);
 export const hasSelectedPeriod = writable(false);
 export const selectedPeriod = writable("AM");
 
+/* === INTERNAL VARIABLES ================= */
+let _hasSelectedPeriod = false;
+
+updateDate(); // update all date stores immedietly
+
 /* === LOCAL STORAGE ====================== */
 if (browser) {
     
 }
 
 /* === UPDATES ============================ */
-const dateInterval = setInterval(() => {
-    // update data every second
+function updateDate() {
+    // create new Date() and get NY time in 24 hour format (0-23)
     let date = new Date();
     let hours24 = date.toLocaleString("en-US", { timeZone: "America/New_York", hour: 'numeric', hour12: false });
-    hours24 < 12 ? period.set('AM') : period.set('PM');
+
+    // set period
+    hours24 < 12 || hours24 == 24 ? period.set('AM') : period.set('PM');
+    if (!_hasSelectedPeriod) hours24 < 12 || hours24 == 24 ? selectedPeriod.set('AM') : selectedPeriod.set('PM');
+
+    // set hours and minutes (NY time)
     hours.set(hours24 % 12);
-    minutes.set(date.toLocaleString("en-US", { timeZone: "America/New_York", minute: 'numeric', hour12: false }));
+    minutes.set(date.toLocaleString("en-US", { timeZone: "America/New_York", minute: 'numeric' }));
+}
+
+const dateInterval = setInterval(() => {
+    // update data every second
+    updateDate();
 }, 1000);
+
+hasSelectedPeriod.subscribe((value) => {
+    // update internal variable
+    _hasSelectedPeriod = value;
+});
 
 selectedPeriod.subscribe((value) => {
     // prevent code from running on server
     if (!browser) return
 
     // set localStorage
-    localStorage.selectedPeriod = value;
+    document.documentElement.setAttribute("data-period", value);
 });
